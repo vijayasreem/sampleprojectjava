89313be36035d5746b93c193f996ab03b4400ab2
kishore <ksaripalli@ctepl.com>
2023-12-14 17:36:23 +0530

    larcas testing3

--- Task.java
+++ Task.java
@@ -1,21 +1,116 @@
-public class Task {
-    private String description;
-    private boolean isCompleted;
+public class DocumentVerificationApp {
+    private String greetingMessage;
+    private boolean isIdentityVerified;
+    private boolean isAddressVerified;
+    private boolean isCreditScoreVerified;
+    private boolean isIncomeVerified;
+    private boolean isVehicleAssessmentPassed;
+    private boolean isPaymentApproved;
+    private String vendorName;
+    private double paymentAmount;
 
-    public Task(String description) {
-        this.description = description;
-        this.isCompleted = false;
+    public DocumentVerificationApp() {
+        this.greetingMessage = "Welcome to the Document Verification App!";
+        this.isIdentityVerified = false;
+        this.isAddressVerified = false;
+        this.isCreditScoreVerified = false;
+        this.isIncomeVerified = false;
+        this.isVehicleAssessmentPassed = false;
+        this.isPaymentApproved = false;
+        this.vendorName = "";
+        this.paymentAmount = 0.0;
     }
 
-    public String getDescription() {
-        return description;
+    public String getGreetingMessage() {
+        return greetingMessage;
     }
 
-    public boolean isCompleted() {
-        return isCompleted;
+    public boolean isIdentityVerified() {
+        return isIdentityVerified;
     }
 
-    public void markAsCompleted() {
-        isCompleted = true;
+    public boolean isAddressVerified() {
+        return isAddressVerified;
+    }
+
+    public boolean isCreditScoreVerified() {
+        return isCreditScoreVerified;
+    }
+
+    public boolean isIncomeVerified() {
+        return isIncomeVerified;
+    }
+
+    public boolean isVehicleAssessmentPassed() {
+        return isVehicleAssessmentPassed;
+    }
+
+    public boolean isPaymentApproved() {
+        return isPaymentApproved;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public double getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public void verifyIdentity(String input) {
+        // Logic to verify identity using provided input
+        // Set isIdentityVerified based on verification result
+    }
+
+    public void verifyAddress(String input) {
+        // Logic to verify address using provided input
+        // Set isAddressVerified based on verification result
+    }
+
+    public void evaluateCreditworthiness(double annualIncome, int creditScore) {
+        // Logic to evaluate creditworthiness based on annual income and credit score
+        // Set isCreditScoreVerified and isIncomeVerified based on evaluation result
+    }
+
+    public void assessVehicleValue(double vehicleAssessmentValue) {
+        // Logic to assess vehicle value
+        // Set isVehicleAssessmentPassed based on assessment result
+    }
+
+    public void approvePayment(double paymentAmount) {
+        // Logic to approve payment based on payment amount
+        // Set isPaymentApproved based on approval result
+    }
+
+    public void disburseFunds() {
+        if (isIdentityVerified && isAddressVerified) {
+            System.out.println("Document verification complete. Eligible for banking services.");
+        } else {
+            System.out.println("Document verification incomplete. Not eligible for banking services.");
+        }
+
+        if (isIncomeVerified && isCreditScoreVerified) {
+            if (annualIncome >= 30000 && creditScore >= 700) {
+                System.out.println("Congratulations! You are eligible for a credit score with a high limit.");
+            } else if (annualIncome >= 20000 && creditScore >= 600) {
+                System.out.println("You are eligible for a credit score with a moderate limit.");
+            }
+        }
+
+        if (isVehicleAssessmentPassed) {
+            if (disbursedAmount <= vehicleAssessmentValue) {
+                System.out.println("Vehicle assessment passed. Disbursed Amount: $" + disbursedAmount);
+            } else {
+                System.out.println("Vehicle assessment failed. Loan amount cannot exceed vehicle value.");
+            }
+        }
+
+        if (isPaymentApproved) {
+            System.out.println("Payment approved.");
+            System.out.println("Vendor: " + vendorName);
+            System.out.println("Payment Amount: $" + paymentAmount);
+        } else {
+            System.out.println("Payment approval required.");
+        }
     }
 }
